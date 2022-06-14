5c02ee6056059b99623db11e3c4ad4865d84c24c
Gabriela Victoria <gabrielavictoria91@gmail.com>
2022-06-14 14:40:41 -0300

    feito 31 e 37 com help

--- secao_03_estrutura_de_repeticao/ex_37_senso_de_academia.py
+++ secao_03_estrutura_de_repeticao/ex_37_senso_de_academia.py
@@ -53,6 +53,97 @@ da média das alturas e dos pesos dos clientes
 
 """
 
+def descobre_mais_alto_e_baixo(cadastro):
+    """
+    Cliente mais alto e mais baixo
+    """
+    nome_do_mais_alto = None
+    nome_do_mais_baixo = None
+    mais_alto = None
+    mais_baixo = None
+
+    for (nome, altura, peso) in cadastro:
+        if mais_alto == None or altura > mais_alto:
+            nome_do_mais_alto = nome
+            mais_alto = altura
+        if mais_baixo == None or altura < mais_baixo:
+            nome_do_mais_baixo = nome
+            mais_baixo = altura
+
+    return (nome_do_mais_alto, mais_alto, nome_do_mais_baixo, mais_baixo)
+
+def descobre_mais_magro_gordo(cadastro):
+    """
+    Cliente mais magro e o mais gordo
+    """
+    nome_do_mais_magro = None
+    nome_do_mais_gordo = None
+    mais_magro = None
+    mais_gordo = None
+
+    for (nome, altura, peso) in cadastro:
+        if mais_magro == None or peso < mais_magro:
+            nome_do_mais_magro = nome
+            mais_magro = peso
+        if mais_gordo == None or peso > mais_gordo:
+            nome_do_mais_gordo = nome
+            mais_gordo = peso
+
+    return (nome_do_mais_magro, mais_magro, nome_do_mais_gordo, mais_gordo)
+
+def descobrir_medias_de_altura_e_peso(cadastro):
+    """
+    Médias dos clientes
+    """
+    total_alturas = 0
+    total_pesos = 0
+    for (nome, altura, peso) in cadastro:
+        total_alturas += altura
+        total_pesos += peso
+
+    media_altura = total_alturas/len(cadastro)
+    media_peso = total_pesos/len(cadastro)
+
+    return (media_altura, media_peso)
+
+
+def obter_input():
+    """
+    Pegando input do teste lá de cima
+    """
+    inputs = []
+    cadastro = []
+    contador = 0
+    while True:
+        inp = input("Insira")
+        inputs.append(inp)
+        contador += 1
+
+        if inp == "0":
+            break
+
+        if contador == 3:
+            nome, altura, peso = inputs
+            cadastro.append([nome, int(altura), int(peso)])
+            inputs = []
+            contador = 0
+    return cadastro
 
 def rodar_senso():
-    """Escreva aqui em baixo a sua solução"""
+    """ """
+    cadastro = obter_input()
+    resultado = descobre_mais_alto_e_baixo(cadastro)
+    nome_do_mais_alto, mais_alto, nome_do_mais_baixo, mais_baixo = resultado
+    resultado = descobre_mais_magro_gordo(cadastro)
+    nome_do_mais_magro, mais_magro, nome_do_mais_gordo, mais_gordo = resultado
+    media_altura, media_peso = descobrir_medias_de_altura_e_peso(cadastro)
+
+    print(f"Cliente mais alto: {nome_do_mais_alto}, com {mais_alto} centímetros")
+    print(f"Cliente mais baixo: {nome_do_mais_baixo}, com {mais_baixo} centímetros")
+    print(f"Cliente mais magro: {nome_do_mais_magro}, com {mais_magro} kilos")
+    print(f"Cliente mais gordo: {nome_do_mais_gordo}, com {mais_gordo} kilos")
+    print("--------------------------------------------------")
+    print(f"Media de altura dos clientes: {media_altura:.1f} centímetros")
+    print(f"Media de peso dos clientes: {media_peso:.1f} kilos")
+
+    #feito com help Roger
